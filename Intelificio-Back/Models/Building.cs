29faa04b5702b5ac768e05e015c5333705a82fc2
Jorge Sandoval <jor.sandoval@duocuc.cl>
2024-09-11 23:32:57 -0300

    Se crea feature Building, metodo Create y Query GetAllByCommunityQuery

--- Intelificio-Back/Models/Building.cs
+++ Intelificio-Back/Models/Building.cs
@@ -5,7 +5,6 @@ namespace Backend.Models
     public class Building : BaseEntity
     {
         public required string Name { get; set; }
-        public bool IsActive { get; set; } = true;
         public required Community Community { get; set; }
         public required int Floors { get; set; }
         public ICollection<Unit> Units { get; set; } = new List<Unit>();
