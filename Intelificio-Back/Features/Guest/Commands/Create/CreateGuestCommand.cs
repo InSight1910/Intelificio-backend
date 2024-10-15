dfbdc08f2a46bee4fe66b25b904233342982ba33
Vicente <vice.espinosa@duocuc.cl>
2024-10-14 23:01:57 -0300

    merge conflicts

--- Intelificio-Back/Features/Guest/Commands/Create/CreateGuestCommand.cs
+++ Intelificio-Back/Features/Guest/Commands/Create/CreateGuestCommand.cs
@@ -2,17 +2,15 @@
 using MediatR;
 using System.Text.Json.Serialization;
 
-namespace Backend.Features.Guest.Commands.Create
+namespace Backend.Features.Guest.Commands.Create;
+
+public class CreateGuestCommand : IRequest<Result>
 {
-    public class CreateGuestCommand : IRequest<Result>
-    {
-        [JsonIgnore]
-        public int? Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Rut { get; set; }
-        public DateTime? EntryTime { get; set; }
-        public string? Plate { get; set; }
-        public int? UnitId { get; set; }
-    }
+    [JsonIgnore] public int? Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Rut { get; set; }
+    public DateTime? EntryTime { get; set; }
+    public string? Plate { get; set; }
+    public int? UnitId { get; set; }
 }
--- Intelificio-Back/Features/Guest/Commands/Update/UpdateGuestCommand.cs
+++ Intelificio-Back/Features/Guest/Commands/Update/UpdateGuestCommand.cs
@@ -2,16 +2,15 @@
 using MediatR;
 using System.Text.Json.Serialization;
 
-namespace Backend.Features.Guest.Commands.Update
+namespace Backend.Features.Guest.Commands.Update;
+
+public class UpdateGuestCommand : IRequest<Result>
 {
-    public class UpdateGuestCommand : IRequest<Result>
-    {
-        [JsonIgnore]
-        public int? Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Rut { get; set; }
-        public string? Plate { get; set; }
-        public int? UnitId { get; set; }
-    }
+    [JsonIgnore] public int? Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Rut { get; set; }
+    public string? Plate { get; set; }
+    public DateTime? EntryTime { get; set; }
+    public int? UnitId { get; set; }
 }
